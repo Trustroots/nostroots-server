b1174a0668d7f9bd41c7fe9d084ff6b043d2bc8a
Simon <abeluga@posteo.net>
2024-08-09 15:11:58 +0200

    derive reposted event

--- validation/repost.ts
+++ validation/repost.ts
@@ -2,8 +2,11 @@ import { nostrToolsRelay } from "../deps.ts";
 const { Relay } = nostrToolsRelay;
 
 import { nostrTools } from "../deps.ts";
-const { finalizeEvent } = nostrTools;
+const { finalizeEvent, generateSecretKey, getPublicKey } = nostrTools;
 type Event = nostrTools.Event;
+type EventTemplate = nostrTools.EventTemplate;
+type VerifiedEvent = nostrTools.VerifiedEvent;
+type Tags = string[][];
 
 import {
   DEV_RELAYS,
@@ -11,11 +14,24 @@ import {
   MAP_NOTE_REPOST_KIND,
 } from "../common/constants.ts";
 
+// should be fixed, not generated new every time
+let SECRET_KEY = generateSecretKey();
+let PUBLIC_KEY = getPublicKey(SECRET_KEY);
+
 const RELAY = DEV_RELAYS[0];
 
 console.log(`connecting to ${RELAY}…`);
 const relay = await Relay.connect(RELAY);
 console.log(`connected to ${relay.url}`);
+
+async function handleEvent(event: Event) {
+  if (!validateEvent(event)) {
+    console.info(`Discarding event…`);
+    return;
+  }
+  const repostedEvent = generateRepostedEvent(event);
+  await publishEvent(repostedEvent);
+}
 
 const sub = relay.subscribe(
   [
@@ -24,9 +40,7 @@ const sub = relay.subscribe(
     },
   ],
   {
-    onevent(event: nostrTools.Event) {
-      console.log("we got the event we wanted:", event);
-    },
+    onevent: handleEvent,
     oneose() {
       sub.close();
     },
@@ -40,14 +54,30 @@ function validateEvent(event: Event) {
   return true;
 }
 
+async function publishEvent(event: VerifiedEvent) {
+  await relay.publish(event);
+}
+
 function generateRepostedEvent(originalEvent: Event) {
-  const originalContent = JSON.stringify(originalEvent);
-  const eventTemplate = {
+  const derivedTags = deriveTags(originalEvent);
+  const derivedContent = deriveContent(originalEvent);
+  const dTag = ["d", `${originalEvent.pubkey}:${originalEvent.id}`];
+  const eTag = ["e", originalEvent.id];
+
+  const eventTemplate: EventTemplate = {
     kind: MAP_NOTE_REPOST_KIND,
     created_at: Math.floor(Date.now() / 1000),
-    tags: [["e", originalEvent.id], ...originalEvent.tags],
-    content: originalContent,
+    tags: [eTag, dTag, ...derivedTags],
+    content: derivedContent,
   };
-  const signedEvent = finalizeEvent(eventTemplate, sk);
+  const signedEvent = finalizeEvent(eventTemplate, SECRET_KEY);
   return signedEvent;
+}
+
+function deriveTags(event: Event): Tags {
+  return event.tags;
+}
+
+function deriveContent(event: Event): string {
+  return event.content;
 }
