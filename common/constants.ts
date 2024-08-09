2a032e2e342200d092b6e49c0f91f445de3d6389
Simon <abeluga@posteo.net>
2024-08-09 14:05:43 +0200

    init

---
+++ common/constants.ts
@@ -0,0 +1,4 @@
+export const RELAY = "wss://nos.lol" as const;
+
+export const OFFER_KIND = 30100 as const;
+export const OFFER_D = "gift:hospitality:offer" as const;
---
+++ validation/repost.ts
@@ -0,0 +1,47 @@
+import { Relay } from "npm:nostr-tools/relay";
+import type { Event } from "npm:nostr-tools";
+
+import { RELAY, OFFER_KIND } from "../common/constants.ts";
+
+console.log(`connecting to ${RELAY}…`);
+const relay = await Relay.connect("wss://nos.lol");
+console.log(`connected to ${relay.url}`);
+
+const sub = relay.subscribe(
+  [
+    {
+      kinds: [OFFER_KIND],
+    },
+  ],
+  {
+    onevent(event: Event) {
+      console.log("we got the event we wanted:", event);
+    },
+    oneose() {
+      sub.close();
+    },
+  }
+);
+
+function validateEvent(event: Event) {
+  return true;
+}
+
+function generateRepostedEvent(event: Event) {
+  const content = JSON.stringify(event);
+  const eventTemplate = {
+    kind: 450,
+    created_at: Math.floor(Date.now() / 1000),
+    tags: [
+      [
+        "a",
+        "34550:<Community event author pubkey>:<d-identifier of the community>",
+        "<Optional relay url>",
+      ],
+      ["e", "<Post Request ID>", "<Optional relay url>"],
+      ["p", "<Post Request Author ID>", "<Optional relay url>"],
+      ["k", "<New Post Request kind>"],
+    ],
+    content: content,
+  };
+}
