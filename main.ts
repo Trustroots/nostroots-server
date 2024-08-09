f0d802153bc264d99b35651f1ebbdd11c46bf06e
Callum Macdonald <git@callum-macdonald.com>
2024-08-09 16:46:26 +0200

    Use cliffy to invoke repost.

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { cliffy, nostrTools } from "./deps.ts";
+import { repost } from "./validation/repost.ts";
+
+function getOrCreatePrivateKey(maybePrivateKeyNsec?: string) {
+  if (typeof maybePrivateKeyNsec === "string") {
+    const decoded = nostrTools.nip19.decode(maybePrivateKeyNsec);
+    if (decoded.type !== "nsec") {
+      throw new Error("#5jLJ2W Invalid nsec");
+    }
+    return decoded.data;
+  }
+
+  const key = nostrTools.generateSecretKey();
+  const nsec = nostrTools.nip19.nsecEncode(key);
+  console.log(`#2yrJza Using random nsec ${nsec}`);
+  return key;
+}
+
+await new cliffy.Command()
+  .globalEnv(
+    "PRIVATE_KEY_NSEC=<value:string>",
+    "Specify the private key in nsec format"
+  )
+  .action((options) => {
+    const privateKey = getOrCreatePrivateKey(options.privateKeyNsec);
+
+    repost(privateKey);
+  })
+  .parse(Deno.args);
--- validation/repost.ts
+++ validation/repost.ts
@@ -2,7 +2,7 @@ import { nostrToolsRelay } from "../deps.ts";
 const { Relay } = nostrToolsRelay;
 
 import { nostrTools } from "../deps.ts";
-const { finalizeEvent, generateSecretKey, getPublicKey } = nostrTools;
+const { finalizeEvent } = nostrTools;
 type Event = nostrTools.Event;
 type EventTemplate = nostrTools.EventTemplate;
 type VerifiedEvent = nostrTools.VerifiedEvent;
@@ -14,38 +14,11 @@ import {
   MAP_NOTE_REPOST_KIND,
 } from "../common/constants.ts";
 
-// should be fixed, not generated new every time
-let SECRET_KEY = generateSecretKey();
-let PUBLIC_KEY = getPublicKey(SECRET_KEY);
-
 const RELAY = DEV_RELAYS[0];
 
 console.log(`connecting to ${RELAY}…`);
 const relay = await Relay.connect(RELAY);
 console.log(`connected to ${relay.url}`);
-
-async function handleEvent(event: Event) {
-  if (!validateEvent(event)) {
-    console.info(`Discarding event…`);
-    return;
-  }
-  const repostedEvent = generateRepostedEvent(event);
-  await publishEvent(repostedEvent);
-}
-
-const sub = relay.subscribe(
-  [
-    {
-      kinds: [MAP_NOTE_KIND],
-    },
-  ],
-  {
-    onevent: handleEvent,
-    oneose() {
-      sub.close();
-    },
-  }
-);
 
 function validateEvent(event: Event) {
   if (!event.kind == MAP_NOTE_KIND) {
@@ -58,7 +31,7 @@ async function publishEvent(event: VerifiedEvent) {
   await relay.publish(event);
 }
 
-function generateRepostedEvent(originalEvent: Event) {
+function generateRepostedEvent(originalEvent: Event, privateKey: Uint8Array) {
   const derivedTags = deriveTags(originalEvent);
   const derivedContent = deriveContent(originalEvent);
   const dTag = ["d", `${originalEvent.pubkey}:${originalEvent.id}`];
@@ -70,7 +43,7 @@ function generateRepostedEvent(originalEvent: Event) {
     tags: [eTag, dTag, ...derivedTags],
     content: derivedContent,
   };
-  const signedEvent = finalizeEvent(eventTemplate, SECRET_KEY);
+  const signedEvent = finalizeEvent(eventTemplate, privateKey);
   return signedEvent;
 }
 
@@ -80,4 +53,25 @@ function deriveTags(event: Event): Tags {
 
 function deriveContent(event: Event): string {
   return event.content;
+}
+
+export async function repost(privateKey: Uint8Array) {
+  const sub = relay.subscribe(
+    [
+      {
+        kinds: [MAP_NOTE_KIND],
+        since: Math.floor(Date.now() / 1e3),
+      },
+    ],
+    {
+      onevent: (event) => {
+        if (!validateEvent(event)) {
+          console.info(`Discarding event…`);
+          return;
+        }
+        const repostedEvent = generateRepostedEvent(event, privateKey);
+        publishEvent(repostedEvent);
+      },
+    }
+  );
 }
