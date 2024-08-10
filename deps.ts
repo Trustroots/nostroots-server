9db3c1ee0dab7f09aacd660f5a57f524d9565517
Simon <abeluga@posteo.net>
2024-08-10 18:04:10 +0200

    set version of nostrify

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
 export * as cliffy from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
-export * as nostrify from "jsr:@nostrify/nostrify";
+export * as nostrify from "jsr:@nostrify/nostrify@^0.30.0";
